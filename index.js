109ff508c574c7508b6dbc27ee91084c304bab58
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2023-12-25 06:34:26 +0100

    add display-container html and styling

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const startBtn = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
 const exitButton = document.querySelector(".exit");
 const continueButton = document.querySelector(".restart");
+const quizContainer = document.querySelector("#display-container");
 
 
 
 startBtn.onclick = () =>{
-    infoBox.style.display = "block";
+    infoBox.style.display = "block"
 }
 exitButton.onclick = () =>{
-    infoBox.style.display = "none"
-}    
+    infoBox.style.display ="none"
+}
+continueButton.onclick = () =>{
+quizContainer.style.display = "block"
+infoBox.style.display ="none"
+}  
+
