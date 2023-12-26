9ea497809a240664dca9ff1667a7778b54523eb5
EniolaCodes <107040860+EniolaCodes@users.noreply.github.com>
2023-12-26 12:56:10 +0100

    add mcq.txt

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const startBtn = document.querySelector(".start-btn button");
+const startButton = document.querySelector(".start-btn button");
 const infoBox = document.querySelector(".info-box");
 const exitButton = document.querySelector(".exit");
 const continueButton = document.querySelector(".restart");
 const quizContainer = document.querySelector("#display-container");
+const timeLeft = document.querySelector(".time-left");
+const countOfQuestion = document.querySelector(".number-of-question");
+const quizBox = document.querySelector("#quiz-box");
+const nextButton = document.querySelector("#next-button");
+const scoreCountainer = document.querySelector(".score-container");
+const userScore = document.querySelector("#user-score")
+const restartButton = document.querySelector("#restart");
+const scoreCount = 0;
+const count = 11;
+let questionCount ;
+let countdown ;
 
 
-
-startBtn.onclick = () =>{
+startButton.onclick = () =>{
     infoBox.style.display = "block"
 }
 exitButton.onclick = () =>{
